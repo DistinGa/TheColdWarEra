8e4c1ed2b2e444624da1656627c8c00cc14985d0
Дмитрий Вавилов <dvav@mail.ru>
2016-07-26 23:16:58 +0700

    Добавлен сплешскрин с логотипом.

---
+++ TheColdWarEra/Assets/Scripts/LogoScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoScript : MonoBehaviour {
+
+	void Start () {
+        Invoke("StartAnimation", 1);
+        Invoke("LoadStartMenu", 5);
+	}
+
+    void StartAnimation()
+    {
+        GetComponent<Animator>().SetTrigger("Start");
+    }
+
+    void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
